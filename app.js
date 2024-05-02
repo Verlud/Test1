2e2822ea0656225cdea72f385fd7ae6e73bec1dc
Verlud <95130861+Verlud@users.noreply.github.com>
2024-05-02 21:28:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(session({
 // Connexion à la base de données MySQL
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'user',
-  password: 'Ludo.310',
-  database: 'Test1'
+  user: 'votre_utilisateur',
+  password: 'votre_mot_de_passe',
+  database: 'nom_de_votre_base_de_donnees'
 });
 
 db.connect((err) => {
@@ -40,6 +40,38 @@ app.get('/', (req, res) => {
   } else {
     res.send('Veuillez vous connecter pour voir cette page!');
   }
+});
+
+
+// Route pour la page d'inscription
+app.get('/inscription', (req, res) => {
+  res.sendFile(__dirname + '/inscription.html'); // Assurez-vous que le fichier inscription.html existe
+});
+
+// Route pour traiter les données du formulaire d'inscription
+app.post('/inscription', (req, res) => {
+  const nom = req.body.nom;
+  const email = req.body.email;
+  const mdp = req.body.mdp;
+
+  // Hachage du mot de passe avec bcrypt
+  bcrypt.hash(mdp, saltRounds, function(err, hash) {
+    if (err) {
+      console.error('Erreur lors du hachage du mot de passe: ' + err.message);
+      res.send('Erreur lors de l\'inscription.');
+    } else {
+      const query = 'INSERT INTO utilisateurs (nom, email, mot_de_passe) VALUES (?, ?, ?)';
+      db.query(query, [nom, email, hash], (err, result) => {
+        if (err) {
+          console.error('Erreur lors de l\'insertion des données: ' + err.message);
+          res.send('Erreur lors de l\'inscription.');
+        } else {
+          console.log('Inscription réussie avec l\'ID: ' + result.insertId);
+          res.send('Inscription réussie pour ' + nom + ' avec l\'email ' + email);
+        }
+      });
+    }
+  });
 });
 
 // Route pour la page de connexion
